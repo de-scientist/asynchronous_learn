6de0482ff94cf195914c6a95a82b2075fb5376b8
de-scientist <gitaumark502@gmail.com>
2025-10-08 09:53:03 +0300

    use setInterval to learn async in js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//use setTimeout to learn async im js
+//use setTimeout to learn async in js
 setTimeout(function ()  {
   console.log("Good morning Mark");
 }, 5000);
@@ -6,3 +6,9 @@ setTimeout(function ()  {
 console.log("I am blessed and sustained");
 console.log("I am highly favored and grateful to the Lord");
 
+//use setInterval to learn async in js
+let i = 0;
+setInterval(function()  {
+  console.log(`$i = ${i}`);
+  
+}, 1000);
