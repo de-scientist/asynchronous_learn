caf551624381e8cab4bf8c5b97d8b9cd234b2494
de-scientist <gitaumark502@gmail.com>
2025-10-08 09:44:18 +0300

    use setTimeout to learn async im js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+//use setTimeout to learn async im js
+setTimeout(function ()  {
+  console.log("Good morning Mark");
+}, 5000);
+
+console.log("I am blessed and sustained");
+console.log("I am highly favored and grateful to the Lord");
+
